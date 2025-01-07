981eadad805854b5e22ee2cde5b1023567051a54
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2025-01-07 17:42:34 +0530

    Add files via upload

---
+++ hoopjump.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner S=new Scanner(System.in);
+		int t=S.nextInt();
+		while(t-->0)
+		{
+		    int n=S.nextInt();
+		    System.out.println((n+1)/2);
+		}
+
+	}
+}
